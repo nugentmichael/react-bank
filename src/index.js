79775f920861bb38093242d2646944125a1a94dd
nugentmichael <nugent.michael@gmail.com>
2020-11-04 08:56:05 -0500

    Add Node Sass support.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/main.css';
+import './assets/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
