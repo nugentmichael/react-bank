5a70e832428f196393510afce66e3ae3758def13
nugentmichael <nugent.michael@gmail.com>
2020-11-04 09:06:40 -0500

    Add base Header component.

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header>
+			<img className="bank-logo" alt="logo" />
+			<nav>
+				<ul>
+					<li>Personal</li>
+					<li>Business</li>
+					<li>About</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
