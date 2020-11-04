6c43602e4d54d5ea9516eef6f7dc02dc8f0aaac9
nugentmichael <nugent.michael@gmail.com>
2020-11-04 09:38:48 -0500

    Code cleanup and refactoring for Sass support.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import LoginForm from './LoginForm';
+import Footer from './Footer';
 import './App.scss';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<LoginForm />
+			<Footer />
 		</div>
 	);
 }
