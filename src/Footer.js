fe7b2caaf0d069a6542a88e1f2cee1979cbb17d7
nugentmichael <nugent.michael@gmail.com>
2020-11-16 09:40:22 -0500

    Add style and layout revisions to the Footer component.

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<h6>
+			<h6 className="text-center text-gray-500 text-xs">
+				&copy; {new Date().getFullYear()}&nbsp;
 				<strong>React Bank:</strong> An online bank portal built on
-				React &copy; {new Date().getFullYear()}
+				React. All rights reserved.
 			</h6>
 		</footer>
 	);
