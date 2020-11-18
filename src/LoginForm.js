237e5a081105086e090f335c5138e341c21f7377
nugentmichael <nugent.michael@gmail.com>
2020-11-18 10:25:31 -0500

    Add base form validation functionality to the LoginForm component.

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginForm = () => {
+	const [valid, setValid] = useState(false);
+	const [userError, setUserError] = useState(false);
+	const [passwordError, setPasswordError] = useState(false);
+
+	const formValidation = (e) => {
+		e.target.type === 'text' && e.target.value === ''
+			? setUserError(true)
+			: setUserError(false);
+		e.target.type === 'password' && e.target.value === ''
+			? setPasswordError(true)
+			: setPasswordError(false);
+		!userError && !passwordError ? setValid(true) : setValid(false);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('Submit!');
+	};
+
 	return (
-		<div className="flex items-center justify-around w-full">
+		<div className="flex items-center justify-around w-full py-8">
 			<div>
 				<h3>Welcome to React Bank!</h3>
 				<h4>
@@ -11,7 +30,10 @@ const LoginForm = () => {
 				</h4>
 			</div>
 			<div className="w-full max-w-xs">
-				<form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+				<form
+					className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+					onSubmit={handleSubmit}
+				>
 					<div className="mb-4">
 						<label
 							className="block text-gray-700 text-sm font-bold mb-2"
@@ -20,11 +42,23 @@ const LoginForm = () => {
 							Username
 						</label>
 						<input
-							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							className={
+								'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ' +
+								(userError ? 'border-red-500' : '')
+							}
 							id="username"
 							type="text"
 							placeholder="Username"
+							onChange={formValidation}
 						/>
+						<p
+							className={
+								'text-red-500 text-xs italic ' +
+								(userError ? 'block' : 'hidden')
+							}
+						>
+							Please enter your username.
+						</p>
 					</div>
 					<div className="mb-6">
 						<label
@@ -34,19 +68,31 @@ const LoginForm = () => {
 							Password
 						</label>
 						<input
-							className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+							className={
+								'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ' +
+								(passwordError ? 'border-red-500' : '')
+							}
 							id="password"
 							type="password"
 							placeholder="******************"
+							onChange={formValidation}
 						/>
-						<p className="text-red-500 text-xs italic">
-							Please choose a password.
+						<p
+							className={
+								'text-red-500 text-xs italic error ' +
+								(passwordError ? 'block' : 'hidden')
+							}
+						>
+							Please enter your password.
 						</p>
 					</div>
 					<div className="flex items-center justify-between">
 						<button
-							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-							type="button"
+							className={
+								'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ' +
+								(!valid ? 'opacity-50 cursor-not-allowed' : '')
+							}
+							type="submit"
 						>
 							Sign In
 						</button>
