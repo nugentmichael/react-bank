c5d75e9a5b47f1e26fc5d8226e3bd802d9a63e99
nugentmichael <nugent.michael@gmail.com>
2020-11-25 08:35:13 -0500

    Revise file structure.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 // Components
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Header from './layouts/Header';
+import Main from './layouts/Main';
+import Footer from './layouts/Footer';
 import './App.scss';
 
 function App() {
