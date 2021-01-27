26df92d451b7bd7a7377520fbbb0526d355f1616
nugentmichael <nugent.michael@gmail.com>
2021-01-27 08:59:10 -0500

    Add Link routes for each account in the Portal component.

--- src/layouts/Portal.js
+++ src/layouts/Portal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { useLocation, useHistory, Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import PortalSidebar from '../components/PortalSidebar';
 
 const Portal = (props) => {
@@ -139,109 +139,141 @@ const Portal = (props) => {
 								</div>
 							</li>
 						</Link>
-						<li className="flex my-3 py-3">
-							<div className="flex flex-col justify-center flex-grow">
-								<a href="#">
+						<Link
+							to={{
+								pathname: '/portal/accounts/savings',
+								account: 'High Interest Savings',
+								balance: bankAccounts
+									? bankAccounts['savings']
+									: savings.current,
+							}}
+						>
+							<li className="flex my-3 py-3">
+								<div className="flex flex-col justify-center flex-grow">
 									<span>High Interest Savings</span>
 									<span className="block text-xs">
 										Savings 00034-5678912
 									</span>
-								</a>
-							</div>
-							<div className="flex flex-col justify-center">
-								<p>
-									<span className="text-lg font-normal">
-										$
-										{bankAccounts
-											? bankAccounts[
-													'savings'
-											  ].toLocaleString()
-											: savings.current.toLocaleString()}
-										<sup className="text-xs">CAD</sup>
-									</span>
-								</p>
-							</div>
-						</li>
+								</div>
+								<div className="flex flex-col justify-center">
+									<p>
+										<span className="text-lg font-normal">
+											$
+											{bankAccounts
+												? bankAccounts[
+														'savings'
+												  ].toLocaleString()
+												: savings.current.toLocaleString()}
+											<sup className="text-xs">CAD</sup>
+										</span>
+									</p>
+								</div>
+							</li>
+						</Link>
 					</ul>
 				</div>
 				<div className="mb-8">
 					<h4 className="font-bold">Credit Cards:</h4>
 					<ul>
-						<li className="flex my-3 py-3">
-							<div className="flex flex-col justify-center flex-grow">
-								<a href="#">
+						<Link
+							to={{
+								pathname: '/portal/accounts/creditcard',
+								account: 'Cash Back MasterCard',
+								balance: bankAccounts
+									? bankAccounts['creditCard']
+									: creditCard.current,
+							}}
+						>
+							<li className="flex my-3 py-3">
+								<div className="flex flex-col justify-center flex-grow">
 									<span>Cash Back MasterCard</span>
 									<span className="block text-xs">
 										Credit Card 1234 5678 9123 4567
 									</span>
-								</a>
-							</div>
-							<div className="flex flex-col justify-center">
-								<p>
-									<span className="text-lg font-normal">
-										$
-										{bankAccounts
-											? bankAccounts[
-													'creditCard'
-											  ].toLocaleString()
-											: creditCard.current.toLocaleString()}
-										<sup className="text-xs">CAD</sup>
-									</span>
-								</p>
-							</div>
-						</li>
+								</div>
+								<div className="flex flex-col justify-center">
+									<p>
+										<span className="text-lg font-normal">
+											$
+											{bankAccounts
+												? bankAccounts[
+														'creditCard'
+												  ].toLocaleString()
+												: creditCard.current.toLocaleString()}
+											<sup className="text-xs">CAD</sup>
+										</span>
+									</p>
+								</div>
+							</li>
+						</Link>
 					</ul>
 				</div>
 				<div className="mb-8">
 					<h4 className="font-bold">Investments:</h4>
 					<ul>
-						<li className="flex my-3 py-3 border-b border-gray-300">
-							<div className="flex flex-col justify-center flex-grow">
-								<a href="#">
+						<Link
+							to={{
+								pathname: '/portal/accounts/rrsp',
+								account: 'Registered Retirement Savings Plan',
+								balance: bankAccounts
+									? bankAccounts['rrsp']
+									: rrsp.current,
+							}}
+						>
+							<li className="flex my-3 py-3 border-b border-gray-300">
+								<div className="flex flex-col justify-center flex-grow">
 									<span>RRSP</span>
 									<span className="block text-xs">
 										Registered Retirement Savings Plan
 										00056-7891234
 									</span>
-								</a>
-							</div>
-							<div className="flex flex-col justify-center">
-								<p>
-									<span className="text-lg font-normal">
-										$
-										{bankAccounts
-											? bankAccounts[
-													'rrsp'
-											  ].toLocaleString()
-											: rrsp.current.toLocaleString()}
-										<sup className="text-xs">CAD</sup>
-									</span>
-								</p>
-							</div>
-						</li>
-						<li className="flex my-3 py-3">
-							<div className="flex flex-col justify-center flex-grow">
-								<a href="#">
+								</div>
+								<div className="flex flex-col justify-center">
+									<p>
+										<span className="text-lg font-normal">
+											$
+											{bankAccounts
+												? bankAccounts[
+														'rrsp'
+												  ].toLocaleString()
+												: rrsp.current.toLocaleString()}
+											<sup className="text-xs">CAD</sup>
+										</span>
+									</p>
+								</div>
+							</li>
+						</Link>
+						<Link
+							to={{
+								pathname: '/portal/accounts/tfsa',
+								account: 'Tax-Free Savings Account',
+								balance: bankAccounts
+									? bankAccounts['tfsa']
+									: tfsa.current,
+							}}
+						>
+							<li className="flex my-3 py-3">
+								<div className="flex flex-col justify-center flex-grow">
 									<span>TFSA</span>
 									<span className="block text-xs">
 										Tax-Free Savings Account 00089-5678912
 									</span>
-								</a>
-							</div>
-							<div className="flex flex-col justify-center">
-								<p>
-									<span className="text-lg font-normal">
-										$
-										{bankAccounts
-											? bankAccounts[
-													'tfsa'
-											  ].toLocaleString()
-											: tfsa.current.toLocaleString()}
-										<sup className="text-xs">CAD</sup>
-									</span>
-								</p>
-							</div>
-						</li>
+								</div>
+								<div className="flex flex-col justify-center">
+									<p>
+										<span className="text-lg font-normal">
+											$
+											{bankAccounts
+												? bankAccounts[
+														'tfsa'
+												  ].toLocaleString()
+												: tfsa.current.toLocaleString()}
+											<sup className="text-xs">CAD</sup>
+										</span>
+									</p>
+								</div>
+							</li>
+						</Link>
 					</ul>
 				</div>
 			</div>
