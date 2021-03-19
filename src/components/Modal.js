b3259119f5fbf576fdb816e1ac9d3b3ea61bb1f7
nugentmichael <nugent.michael@gmail.com>
2021-03-19 08:48:19 -0400

    Remove unnecessary markup in the Modal component.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,41 +5,39 @@ const Modal = (props) => {
 	const [openModal, setOpenModal] = useState(true);
 
 	return (
-		<div className="w-full fixed top-0 right-0 bottom-0 left-0 z-50 bg-blue-100 bg-opacity-90">
-			<section className="flex items-center justify-center h-full">
-				<div className="flex flex-col items-center justify-center p-4 bg-white w-3/6 h-1/5 border-4 border-blue-500 border-opacity-30 rounded-lg">
-					<div className="my-4">
-						<p className="text-l">{message}</p>
-					</div>
-
-					<div>
-						{func && (
-							<button
-								className="mr-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-								onClick={() => {
-									func && func !== (false || null)
-										? func()
-										: setOpenModal(!openModal);
-								}}
-							>
-								Proceed
-							</button>
-						)}
-						{cancel && (
-							<button
-								className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-								onClick={() => {
-									cancel && cancel !== (false || null)
-										? cancel()
-										: setOpenModal(!openModal);
-								}}
-							>
-								Cancel
-							</button>
-						)}
-					</div>
+		<div className="w-full fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-blue-100 bg-opacity-90">
+			<div className="flex flex-col items-center justify-center p-4 bg-white w-3/6 border-4 border-blue-500 border-opacity-30 rounded-lg">
+				<div className="my-4">
+					<p className="text-l">{message}</p>
 				</div>
-			</section>
+
+				<div>
+					{func && (
+						<button
+							className="mr-3 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							onClick={() => {
+								func && func !== (false || null)
+									? func()
+									: setOpenModal(!openModal);
+							}}
+						>
+							Proceed
+						</button>
+					)}
+					{cancel && (
+						<button
+							className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							onClick={() => {
+								cancel && cancel !== (false || null)
+									? cancel()
+									: setOpenModal(!openModal);
+							}}
+						>
+							Cancel
+						</button>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
