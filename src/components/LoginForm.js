d9ee68d522427891822fda070004c77cc8011c4f
nugentmichael <nugent.michael@gmail.com>
2021-03-24 09:11:17 -0400

    Restore the tooltip bottom arrow SVG in the LoginForm component.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -114,6 +114,18 @@ const LoginForm = () => {
 						<div className="relative">
 							<div className="bg-black text-white text-xs rounded py-1 px-4 right-0 bottom-full">
 								Type in any username and password.
+								<svg
+									className="absolute text-black h-2 w-full left-0 top-full"
+									x="0px"
+									y="0px"
+									viewBox="0 0 255 255"
+									xmlSpace="preserve"
+								>
+									<polygon
+										className="fill-current"
+										points="0,0 127.5,127.5 255,0"
+									/>
+								</svg>
 							</div>
 						</div>
 					</div>
